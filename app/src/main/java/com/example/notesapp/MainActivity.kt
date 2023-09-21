a0e97fc676eec56bfe85b40b59fdc7a25f9e4927
H.Rajapaksha <it20179458@my.sliit.lk>
2023-09-21 20:55:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -0,0 +1,147 @@
+package com.example.notesapp
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.LinearLayout
+import android.widget.PopupMenu
+import android.widget.SearchView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.cardview.widget.CardView
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.example.notesapp.Adapter.NotesAdapter
+import com.example.notesapp.Database.NoteDatabase
+import com.example.notesapp.Models.Note
+import com.example.notesapp.Models.NotesViewModel
+import com.example.notesapp.databinding.ActivityMainBinding
+import java.util.*
+
+class MainActivity : AppCompatActivity(), NotesAdapter.NotesClickListener,
+
+    PopupMenu.OnMenuItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: NoteDatabase
+    lateinit var viewModel: NotesViewModel
+    lateinit var adapter: NotesAdapter
+    lateinit var selectedNote: Note
+
+    private val updateNote =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val note = result.data?.getSerializableExtra("note") as? Note
+                if (note != null) {
+                    viewModel.updateNote(note)
+                }
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        //initializing UI
+        initUI()
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(NotesViewModel::class.java)
+
+        viewModel.allNotes.observe(this) { list ->
+            list?.let {
+                adapter.updateList(list)
+            }
+        }
+
+        database = NoteDatabase.getDatabase(this)
+    }
+
+    override fun onResume() {
+        if (binding.searchView != null) {
+            binding.searchView.clearFocus()
+            binding.searchView.setQuery(
+                "", false
+            )
+        }
+
+        super.onResume()
+    }
+
+    private fun initUI() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager = StaggeredGridLayoutManager(2, LinearLayout.VERTICAL)
+        adapter = NotesAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val note = result.data?.getSerializableExtra("note") as? Note
+                    if (note != null) {
+                        viewModel.insertNote(note)
+                    }
+                }
+
+            }
+
+        binding.fbAddNote.setOnClickListener {
+            val intent = Intent(this, AddNoteActivity::class.java)
+            getContent.launch(intent)
+        }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText != null) {
+                    adapter.filterList(newText)
+                }
+                return true
+            }
+
+        })
+    }
+
+    override fun onItemClicked(note: Note) {
+        val intent = Intent(this@MainActivity, AddNoteActivity::class.java)
+        intent.putExtra("current_note", note)
+        updateNote.launch(intent)
+    }
+
+    override fun onLongItemClicked(note: Note, cardView: CardView) {
+        selectedNote = note
+        popUpDisplay(cardView)
+    }
+
+    private fun popUpDisplay(cardView: CardView) {
+        val popUp = PopupMenu(this, cardView)
+        popUp.setOnMenuItemClickListener(this@MainActivity)
+        popUp.inflate(R.menu.pop_up_menu)
+        popUp.show()
+
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        if (item?.itemId == R.id.delete_note) {
+            viewModel.deleteNode(selectedNote)
+            return true
+        }
+        return false
+    }
+}
+
+
+
+
+
